fc3dfb663915a1b8c3917998fc80a061b34bd455
Practice <69354611+RMZ0829@users.noreply.github.com>
2020-08-07 22:15:57 +0800

    Add files via upload

---
+++ text.c
@@ -0,0 +1,119 @@
+//#include <stdio.h>
+
+//int main()
+//{
+//	char ch = 'A';
+//	printf("%c\n",ch);
+//	int age = 17;
+//	printf("%d\n", age);
+//	float f = 5.0;
+//	printf("%f\n", f);
+//	double d = 3.14;
+//	printf("%lf\n", d);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	short age = 17;
+//	float weight = 60.0f;
+//	char ch = 'w';
+//	printf("%d\n", age);
+//	printf("%f\n", weight);
+//	printf("%c\n", ch);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int sum = 0;
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf_s("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf_s("%d%d", &num1, &num2);
+//	sum = num1 * num2;
+//	printf("%d", sum);
+//	return 0;
+//}
+
+
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf_s("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("%d", sum);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char ch = 'A';
+//	printf("%c\n", ch);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	float f = 4.9;
+//	printf("%f\n", f);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf_s("%d%d", &num1, &num2);
+//	sum = num1 * num2;
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf_s("%d%d", &num1, &num2);
+//	sum = num1 - num2;
+//	printf("%d\n", sum);
+//	return 0;
+//}
